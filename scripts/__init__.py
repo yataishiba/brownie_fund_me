e0a334650a39196dedddd0ad6d76d4738241d434
yabaishiba <yutomizu0905@gmail.com>
2022-02-01 18:46:17 +0000

    first commit

---
+++ scripts/__init__.py
@@ -0,0 +1,9 @@
+from brownie import FundMe
+
+
+def deploy_fund_me():
+    account = get_account()
+
+
+def main():
+    deploy_fund_me()
---
+++ scripts/deploy.py
@@ -0,0 +1,25 @@
+from brownie import FundMe, config, network, MockV3Aggregator
+from scripts.helpful_scripts import deploy_mocks, get_account, LOCAL_BLOCKCHAIN_ENV
+from web3 import Web3
+
+
+def deploy_fund_me():
+    account = get_account()
+
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENV:
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+    else:
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address
+        # print(price_feed_address)
+
+    fund_me = FundMe.deploy(price_feed_address, {
+                            "from": account}, publish_source=config["networks"][network.show_active()].get("verify"))
+    print(f"Contract deployed to {fund_me.address}")
+    return fund_me
+
+
+def main():
+    deploy_fund_me()
